f51a368f5c43864688ffa55af310b5e0ff2d6f9e
adferove <adferove@hotmail.com>
2021-09-08 19:55:45 +1000

    Create myFirstAction.js

---
+++ Actions/myFirstAction.js
@@ -0,0 +1,5 @@
+console.log("Welcome to BenkoBot "+new Trellinator().username());
+console.log("Welcome to BenkoBot "+new Trellinator().username());
+console.log("Welcome to BenkoBot "+new Trellinator().username());
+console.log("Welcome to BenkoBot "+new Trellinator().username());
+console.log("Welcome to BenkoBot "+new Trellinator().username());
