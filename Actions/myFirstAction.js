5e6074fe0e5570e059f3f9de9a01b2f9e1c101ee
adferove <adferove@hotmail.com>
2021-09-08 23:42:43 +1000

    Update myFirstAction.js

--- Actions/myFirstAction.js
+++ Actions/myFirstAction.js
@@ -3,29 +3,5 @@ const sumSomething = (a,b)=>{
   return a + b;
 }
 
-//Welcome to BenkoBot! Here's the documentation:
-//http://docs.trellinator.com/
-console.log("Welcome to BenkoBot "+new Trellinator().username());
 
-// ------------ Scheduled Actions ------------ //
-//
-// To schedule an Action to run in the future:
-//
-// Step 1: Make an Action and save it.
-//
-// You can use the following arguments in your Action:
-//
-//    ___queue.args().params
-//    ___queue.args().signature
-//    ___queue.args().original_date
-//
-// Step 2: In another Action, or in a Listener, schedule the Action:
-//
-//.   ___queue.put("youGotScheduled",{id: cr8d.id()},Trellinator.now().addMinutes(1));
-//
-// Step 3: There is no step 3 - unless you want your action to run again**
-//
-// There is no repeat. You need to schedule your saved Action again in your saved Action. You know, like Inception.
-//
-// ------------ / Scheduled Actions ------------ //
 
