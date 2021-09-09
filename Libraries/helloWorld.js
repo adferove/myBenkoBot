19d2e19f6ada1bf96214f25a5489de5bb0243115
adferove <adferove@hotmail.com>
2021-09-09 11:50:58 +1000

    Update helloWorld.js

--- Libraries/helloWorld.js
+++ Libraries/helloWorld.js
@@ -1,8 +1,13 @@
-const Main = () => (
-  <Router>
-    <Home path="/" />
-    <About path="/about" />
-    // Advanced is an optional query
-    <Search path="/search/:query/:advanced?" />
-  </Router>
-);
+// @flow
+
+export default (
+  actionItem /*: Action */,
+  polling /*: boolean */,
+  action /*: Array<AppStateAction> */,
+) /*: function */ => (item) /*: Action */ => {
+  if (item.actionName === actionItem.actionName) {
+    item.polling = polling;
+    item.runningJobs = action;
+  }
+  return item;
+};
